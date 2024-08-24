500ba364bca2266be452bd2450cdff6ca9df254e
RAHUL BISWAL <72703616+Rahul-Biswal-new@users.noreply.github.com>
2024-08-24 16:23:53 -0400

    features/setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
 module.exports = {
   content: [],
+  content: ["./app/**/* .{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
