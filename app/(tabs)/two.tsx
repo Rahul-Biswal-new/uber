4cc328d1041e8692d5130837056d2cbf2ac88a02
RAHUL BISWAL <72703616+Rahul-Biswal-new@users.noreply.github.com>
2024-08-26 13:17:39 -0400

    features/layout

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,14 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { StyleSheet, View,Text } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <View style={styles.separator}/>
+      {/* <EditScreenInfo path="app/(tabs)/two.tsx" /> */}
     </View>
   );
 }
--- app/modal.tsx
+++ app/modal.tsx
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
+import { Stack } from "expo-router";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+
 
 export default function ModalScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
-
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
-    </View>
+    <Stack>
+          <Stack.Screen name="index"  />
+    </Stack>
   );
 }
+
+{/* <Stack.Screen name="index" options={{ headerShown: false }} />
+<Stack.Screen name="(auth)" options={{ headerShown: false }} />
+<Stack.Screen name="(root)" options={{ headerShown: false }} />
+<Stack.Screen name="+not-found" /> */}
 
 const styles = StyleSheet.create({
   container: {
@@ -33,3 +33,13 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+
+
+{/* <View style={styles.container}>
+<Text style={styles.title}>Modal</Text>
+<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+<EditScreenInfo path="app/modal.tsx" /> */}
+
+{/* Use a light status bar on iOS to account for the black space above the modal */}
+{/* <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+</View> */}
